8e6fb726d44a132b3e9b1d13e188da1dc69e0e33
AayushGupta56 <80825841+AayushGupta56@users.noreply.github.com>
2022-06-19 02:52:14 +0530

    Update 16.Partition a set into two subsets such that the difference of subset sums is minimum.cpp

--- 16.Partition a set into two subsets such that the difference of subset sums is minimum.cpp
+++ 16.Partition a set into two subsets such that the difference of subset sums is minimum.cpp
@@ -1,7 +1,7 @@
 int minSubsetSumDifference(vector<int>& arr, int n)
 {
-	// Write your code here.
-       if(n==1){return arr[0];}
+    // Write your code here.
+     //  if(n==1){return arr[0];}
     
     int total=0;int ans=1e9;
     for(int i=0;i<n;i++)total=total+arr[i];
@@ -12,26 +12,26 @@ int minSubsetSumDifference(vector<int>& arr, int n)
     if(arr[0]<=total)dp[0][arr[0]]=true;
     
     for(int i=1;i<n;i++){
-        for(int target=0;target<=total;target++){
+        for(int target=1;target<=total;target++){
             bool pick=false;bool notpick=false;
             
             if(arr[i]<=target)pick=dp[i-1][target-arr[i]];
             
             notpick=dp[i-1][target];
-          //if we have made the target then we will take the difference of two subsets
-            //because the target will be made of some numbers which will  make a unique subset 
-            if(pick){
-                int temp=abs(target-(total-target));
-                ans=min(ans,temp);
-            }
+       
+           
             
-             if(notpick){
-                int temp=abs(target-(total-target));
-                ans=min(ans,temp);
-           }
-            
-            dp[i][target]=pick|notpick;
+            dp[i][target]=pick||notpick;
             }
     }
+       //if we have made the target then we will take the difference of two subsets
+            //because the target will be made of some numbers which will  make a unique subset 
+        for(int i=0;i<=total;i++){
+            if(dp[n-1][i]==true){
+                int s1=i;int s2=total-i;
+                ans=min(ans,abs(s1-s2));
+            }
+        }
+    
     return ans;
 }
